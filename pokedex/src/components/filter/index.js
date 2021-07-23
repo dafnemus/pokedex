f9b2a865a53a72468ce0459d1a1ad33f6aa1da57
dafnemus <dafnemus@gmail.com>
2021-07-23 14:00:45 -0300

    component filter is created

---
+++ pokedex/src/components/filter/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import iconFilter from '../../assets/arrow-down.svg'
+
+function Filter() {
+  return (
+    <a>
+      # <img src={iconFilter} alt="filtro" />
+    </a>
+  );
+}
+
+export default Filter;
