aec5a498631488c809e270d96a56eb2a9ffd6dfe
dafnemus <dafnemus@gmail.com>
2021-07-26 18:30:57 -0300

    styles added in the header

--- pokedex/src/components/App.js
+++ pokedex/src/components/App.js
@@ -4,13 +4,13 @@ import logoPoke from '../assets/pokeball-logo.svg';
 import Filter from './filter/index';
 import SearchBar from './searchBar/index';
 import ContainMainCards from './containMinCards';
-
+import styles from './styles.module.scss'
 
 function App() {
   return (
-    <div>
-      <div>
-        <div>
+    <div className={styles.mainContain}>
+      <div className={styles.containHeader}>
+        <div className={styles.containLogo}>
           <img src={logoPoke} alt='Logo Pokemon' />
           <h1>Pokédex</h1>
         </div>
