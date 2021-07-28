a02063089da5b2a37c62e5549f38e641940602e2
dafnemus <dafnemus@gmail.com>
2021-07-28 15:51:37 -0300

    aplico logica para ordenar lo pokemones por numero y por abc

--- pokedex/src/components/containMinCards/index.js
+++ pokedex/src/components/containMinCards/index.js
@@ -3,7 +3,29 @@ import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 import Pokemon from '../cuadrilla/index';
 
-function ContainMainCards({ onClick, pokemons }) {
+function ContainMainCards({ onClick, pokemons, value }) {
+  if (value) {
+    pokemons.sort(function (a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  if (value === false) {
+    pokemons.sort(function (a, b) {
+      if (a.order < b.order) {
+        return -1;
+      }
+      if (a.order > b.order) {
+        return 1;
+      }
+      return 0;
+    });
+  }
   return (
     <div className={styles.containCards}>
       {pokemons.map((poke) => {
@@ -25,11 +47,13 @@ function ContainMainCards({ onClick, pokemons }) {
 ContainMainCards.propTypes = {
   onClick: PropTypes.func,
   pokemons: PropTypes.array,
+  value: PropTypes.bool,
 };
 
 ContainMainCards.defaultProps = {
   onClick: null,
   pokemons: [],
+  value: false,
 };
 
 export default ContainMainCards;
