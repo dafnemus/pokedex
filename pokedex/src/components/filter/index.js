6c92faca8813be95d432dd986824d412d0d19ca1
dafnemus <dafnemus@gmail.com>
2021-07-28 15:50:20 -0300

    filtro abc y filtro order layout

--- pokedex/src/components/filter/index.js
+++ pokedex/src/components/filter/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import iconFilter from '../../assets/arrow-down.svg';
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 
-function Filter() {
+function Filter({ onClick, value }) {
   return (
-    <div className={styles.containFilter}>
-      <a className={styles.filter}>#</a>
+    <div onClick={onClick} className={styles.containFilter}>
+      {value ? (
+        <a className={styles.filter}>A-Z</a>
+      ) : (
+        <a className={styles.filter}>#</a>
+      )}
       <img src={iconFilter} alt='filtro' />
     </div>
   );
 }
+
+Filter.propTypes = {
+  onClick: PropTypes.func,
+  value: PropTypes.bool,
+};
+
+Filter.defaultProps = {
+  onClick: null,
+  value: false,
+};
 
 export default Filter;
